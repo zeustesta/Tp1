ca9743d0698d37e3a5d1176debc0661b61da3c48
zeustesta <zeuserosmardel@hotmail.com.ar>
2022-03-17 20:12:52 -0300

    Tp1

---
+++ main.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+int capitalInvertido = 0;
+float ganancia = 2, gananciaTotal = 0;
+
+int main()
+{
+    printf("Introducir su capital invertidon\n");
+    scanf("%i", &capitalInvertido);
+    ganancia = (capitalInvertido*2)/100;
+    gananciaTotal = capitalInvertido+ganancia;
+    printf("Su ganancia es de %2.f", gananciaTotal);
+}
+*/
+
+/*
+int pagoInicial = 0;
+int descuento = 0;
+float pagoFinal = 0;
+
+int main()
+{
+    printf("Ingrese el monto de compra: \n");
+    scanf("%i", &pagoInicial);
+    descuento = (pagoInicial*15)/100;
+    pagoFinal = pagoInicial-descuento;
+    printf("El monto total a pagar es: %2.f", pagoFinal);
+}
+*/
+
+/*
+int totalAlumnosVarones = 0, total = 0, totalAlumnosMujeres = 0;
+int porcentajeHombres = 0, porcentajeMujeres = 0;
+
+int main()
+{
+    printf("Ingrese el total de alumnos varones\n");
+    scanf("%i", &totalAlumnosVarones);
+    printf("Ingrese el total de mujeres\n");
+    scanf("%i", &totalAlumnosMujeres);
+    total = totalAlumnosMujeres+totalAlumnosVarones;
+    porcentajeHombres = totalAlumnosVarones*100/total;
+    porcentajeMujeres = totalAlumnosMujeres*100/total;
+    printf("El porcentaje de mujeres es:%i%% \n", porcentajeMujeres);
+    printf("El porcentaje de hombres es:%i%%", porcentajeHombres);
+}
+*/
+
+/*int main()
+{
+    float sumaTotal = 0, promedio = 0, nota = 0;
+
+    printf("Ingrese la primer nota\n");
+    scanf("%f", &nota);
+    sumaTotal = sumaTotal+nota;
+
+    printf("Ingrese la segunda nota\n");
+    scanf("%f", &nota);
+    sumaTotal = sumaTotal+nota;
+
+    printf("Ingrese la tercer nota\n");
+    scanf("%f", &nota);
+    sumaTotal = sumaTotal+nota;
+
+    promedio = sumaTotal/3;
+
+    if(promedio>=7)
+        printf("Usted aproboó con: %.2f%%", promedio);
+    else
+        printf("Usted desaprobo con %.2f%%", promedio);
+} */
+
+/*int main()
+{
+    float compraInicial = 0, compraFinal = 0, descuento = 0;
+
+    printf("Ingrese el monto de la compra\n");
+    scanf("%f", &compraInicial);
+
+    descuento = (compraInicial*20)/100;
+
+
+
+
+    if(compraInicial>5000)
+    {
+        compraFinal = compraInicial-descuento;
+        printf("El pago final seria de: %.1f, obtuvo un descuento de: %.1f", compraFinal, descuento);
+    }
+    else
+    {
+        printf("Su compra no supera los 5000, no hay descuento. Debe pagar: %f", compraInicial);
+    }
+} */
+
+/*int main()
+{
+    int horasTrabajadas = 0, horasExtras = 0, sueldoBasico = 0, sueldoTotal = 0;
+
+    printf("Ingrese sus horas trabajadas\n");
+    scanf("%i", &horasTrabajadas);
+
+
+
+    if(horasTrabajadas <=40)
+    {
+        printf("Usted trabajo %i horas, obtuvo un sueldo de %i por hora, y un sueldo total de %i\n", horasTrabajadas, sueldoBasico, (horasTrabajadas*sueldoBasico));
+    }
+    else if(horasTrabajadas>40)
+    {
+        sueldoBasico = (40*300);
+        horasExtras = (horasTrabajadas-40);
+        sueldoTotal = (horasExtras*500)+sueldoBasico;
+        printf("Usted trabajo %i horas, con %i horas extras. Obtuvo un sueldo por hora de %i y un sueldo total de %i", horasTrabajadas, horasExtras, sueldoBasico, sueldoTotal);
+    }
+} */
+
+int main()
+{
+
+}
+
+
